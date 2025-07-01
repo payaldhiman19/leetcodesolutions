65d215c168d551bcb0c377b8573b77ba4c0df778
Payal dhiman <payaldhiman0742@gmail.com>
2025-07-01 12:43:23 +0530

    Time: 0 ms (100%), Space: 42 MB (88.84%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,6 +1,9 @@
 class Solution {
     public int possibleStringCount(String word) {
         int cnt=1;
+        //it should be 1+total number of characters that can be removed
+        //to remove the how many character we need theri cnt so 1+count of number repeating here
+        // 1-->original string or character
         for(int i=1;i<word.length();i++){
             if(word.charAt(i-1)==word.charAt(i)){
                 cnt++;
