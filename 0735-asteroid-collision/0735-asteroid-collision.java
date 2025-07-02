0d0d7dc36a38b7f9d9fce7946386fc1e18a75c51
Payal dhiman <payaldhiman0742@gmail.com>
2025-07-02 22:57:05 +0530

    Time: 5 ms (85.02%), Space: 45.5 MB (53.14%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,29 +3,28 @@ class Solution {
         int n=asteroids.length;
         Stack<Integer>st=new Stack<>();
         for(int a:asteroids){
-            boolean destroyed=false;
-            //collisoon w
-    while(!st.isEmpty() &&a<0&&st.peek()>0){
-        //if negative number occurs then pop it
-        if(st.peek()<-a){
-            st.pop();
-            //keep chcking
-            continue;
-        }else if(st.peek()==-a){
-            //if both are same then destroy both
-            st.pop();
+            boolean destroy=false;
+            while(!st.isEmpty() &&a<0 &&st.peek()>0){
+                //check if negative is smaller or grater 
+                if(st.peek()<-a){
+                st.pop();
+                continue;
+                //keep checking
+                }else if(st.peek()==-a){
+                    st.pop();
+                }
+                destroy=true;
+                break;
+            }
+            if(!destroy){
+            st.push(a);
+            }   
         }
-        destroyed=true;
-        break;
-    }
-    if(!destroyed){
-        st.push(a);
-    }
+        int res[]=new int[st.size()];
+        for(int i=res.length-1;i>=0;i--){
+            res[i]=st.pop();
         }
-int res[]=new int[st.size()];
- for(int i=res.length-1;i>=0;i--){
-    res[i]=st.pop();
- }
- return res;
+        return res;
+
     }
 }
