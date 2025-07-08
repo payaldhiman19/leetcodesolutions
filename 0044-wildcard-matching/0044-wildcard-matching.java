2a9ed6996bcbfd8b9a7dad90d501582ab818d1b4
Payal dhiman <payaldhiman0742@gmail.com>
2025-07-08 15:40:33 +0530

    Time: 19 ms (62.27%), Space: 44.7 MB (76.98%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,38 +1,28 @@
 class Solution {
-    public boolean helper(String s,String t,int i,int j,Boolean dp[][]){
-        //base case: if all are processed
-        if(i<0&&j<0){
-            return true;
-        }
-        //if text is still there
-        if(j<0) return false;
-        if(i<0){
-                       // Remaining pattern should be all '*'
-
-for(int k=0;k<=j;k++){
-    if(t.charAt(k)!='*')
-    return false;
-}
-return true;   //if * is present it can match any multiple character
-        }
-          if (dp[i][j] != null) return dp[i][j];
-        char tc=s.charAt(i);
-       char pc=t.charAt(j);
-       if(pc=='*'){
-        //mathes muitiple charactes
-        dp[i][j]=helper(s,t,i-1,j,dp) ||helper(s,t,i,j-1,dp);
-       }else if(pc==tc || pc=='?'){
-        //matxches characters one by one then move i and j pointer
-        dp[i][j]=helper(s,t,i-1,j-1,dp);
-       }else{
-        dp[i][j]=false;
-       }
-       return dp[i][j];
-    }
     public boolean isMatch(String s, String p) {
-        int m=s.length();
-        int n=p.length();
-        Boolean [][]dp=new  Boolean[m][n];
-      return helper(s,p,m-1,n-1,dp);
-}
+         int m = s.length(), n = p.length();
+    boolean[][] dp = new boolean[m + 1][n + 1];
+
+    dp[0][0] = true; // empty matches empty
+
+    // Fill for pattern starts with *
+    for (int j = 1; j <= n; j++) {
+        if (p.charAt(j - 1) == '*')
+            dp[0][j] = dp[0][j - 1];
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            char sc = s.charAt(i - 1);
+            char pc = p.charAt(j - 1);
+
+            if (pc == '?' || pc == sc)
+                dp[i][j] = dp[i - 1][j - 1];
+            else if (pc == '*')
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+        }
+    }
+
+    return dp[m][n];
+    }
 }
