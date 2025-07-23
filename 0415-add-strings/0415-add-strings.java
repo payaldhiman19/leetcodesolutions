19624b9be252042bff76811200c6a3496ada308f
Payal dhiman <payaldhiman0742@gmail.com>
2025-07-23 12:38:32 +0530

    Time: 2 ms (59.66%), Space: 42.2 MB (97.65%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -5,22 +5,24 @@ class Solution {
        int carry=0;
        int i=n-1,j=m-1;
        StringBuilder ans=new StringBuilder();
-       while(i>=0 && j>=0){
-        int sum=(num1.charAt(i--)-'0')+(num2.charAt(j--)-'0')+carry;
+       while(i>=0 || j>=0){
+            int n1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j--) - '0' : 0;
+            int sum = n1 + n2 + carry;
         ans.append(sum%10);
         carry=sum/10;
        } 
        //if i completes or j
-       while(i>=0){
-        int sum=(num1.charAt(i--)-'0')+carry;
-        ans.append(sum%10);
-        carry=sum/10;
-       }
-        while(j>=0){
-        int sum=(num2.charAt(j--)-'0')+carry;
-        ans.append(sum%10);
-        carry=sum/10;
-       }
+    //    while(i>=0){
+    //     int sum=(num1.charAt(i--)-'0')+carry;
+    //     ans.append(sum%10);
+    //     carry=sum/10;
+    //    }
+    //     while(j>=0){
+    //     int sum=(num2.charAt(j--)-'0')+carry;
+    //     ans.append(sum%10);
+    //     carry=sum/10;
+    //    }
     if(carry!=0){
         ans.append(carry);
     }
