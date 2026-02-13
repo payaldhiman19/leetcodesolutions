263aa50877a02da503e26f39f2c054318296a3e1
Payal dhiman <payaldhiman0742@gmail.com>
2026-02-13 13:27:01 +0530

    Time: 2 ms (98.17%), Space: 43.8 MB (88.83%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,31 +1,24 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-       int n=num1.length();
-       int m=num2.length();
+       //same as we did add binary strings thing
+       int i=num1.length()-1;
+       int j=num2.length()-1;
        int carry=0;
-       int i=n-1,j=m-1;
-       StringBuilder ans=new StringBuilder();
-       while(i>=0 || j>=0){
-            int n1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
-            int n2 = j >= 0 ? num2.charAt(j--) - '0' : 0;
-            int sum = n1 + n2 + carry;
-        ans.append(sum%10);
+       StringBuilder sb=new StringBuilder();
+       while(i>=0||j>=0||carry>0){
+        int a=0,b=0;
+        if(i>=0){
+            a=num1.charAt(i)-'0';
+            i--;
+        }
+        if(j>=0){
+            b=num2.charAt(j)-'0';
+            j--;
+        }
+        int sum=a+b+carry;
+        sb.append(sum%10);
         carry=sum/10;
        } 
-       //if i completes or j
-    //    while(i>=0){
-    //     int sum=(num1.charAt(i--)-'0')+carry;
-    //     ans.append(sum%10);
-    //     carry=sum/10;
-    //    }
-    //     while(j>=0){
-    //     int sum=(num2.charAt(j--)-'0')+carry;
-    //     ans.append(sum%10);
-    //     carry=sum/10;
-    //    }
-    if(carry!=0){
-        ans.append(carry);
-    }
-    return ans.reverse().toString();
+       return sb.reverse().toString();
     }
 }
