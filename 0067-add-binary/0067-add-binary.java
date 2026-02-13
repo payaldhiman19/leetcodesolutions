a62b11bf3520bb35f42b8f3ff6b910fb3157e0e1
Payal dhiman <payaldhiman0742@gmail.com>
2026-02-13 13:08:47 +0530

    Time: 1 ms (99.64%), Space: 43.9 MB (25.17%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,18 +1,24 @@
 class Solution {
-    public String addBinary(String a, String b) {
-       int i=a.length()-1;
-       int j=b.length()-1;
-       int carry=0;
-       StringBuilder sb=new StringBuilder();
-       while(i>=0||j>=0||carry!=0){
-        int x=(i>=0)?a.charAt(i)-'0':0;
-        int y=(j>=0)?b.charAt(j)-'0':0;
-        int sum=x+y+carry;
-        //update carry
-        sb.append(sum%2);  //append the last digit
-        carry=sum/2;
-        i--;j--;
-       }
-       return sb.reverse().toString();
+    public String addBinary(String a, String b) { 
+        //check in decimal first  by adding digits from back a+b+carry
+        //carry=sum/2 and result digit that we stire we will get from sum%2 becoz we are adding binary that why by 2 in end reverse res
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        StringBuilder res=new StringBuilder();
+     while(i>=0 ||j>=0||carry==1){
+        int sum=carry; //coz carry nedd to be added
+      if(i>=0){
+        sum+=a.charAt(i)-'0';
+        i--;
+      }
+       if(j>=0){
+        sum+=b.charAt(j)-'0';
+        j--;
+      }
+      res.append(sum%2);
+      carry=sum/2;
+     }
+     return res.reverse().toString();
     }
 }
