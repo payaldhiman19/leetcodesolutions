e7e0e0911af45815c85caf9e33dcad5adafdf6b4
Payal dhiman <payaldhiman0742@gmail.com>
2026-02-14 20:46:39 +0530

    Time: 4 ms (17.73%), Space: 45.7 MB (10.55%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-     Set<List<Integer>>res=new HashSet<>();
-     Arrays.sort(nums);
-     helper(res,new ArrayList<>(),nums,0);
-     return new ArrayList<>(res);
+    public void helper(int start,Set<List<Integer>>res,List<Integer>temp,int nums[]){
+        res.add(new ArrayList<>(temp));
+        for(int i=start;i<nums.length;i++){
+            temp.add(nums[i]);
+            helper(i+1,res,temp,nums);
+            //backtrack
+            temp.remove(temp.size()-1);
+        }
     }
-    public void helper(Set<List<Integer>>res,List<Integer>templist,int nums[],int start){
-     res.add(new ArrayList<>(templist));
-     for(int i=start;i<nums.length;i++){
-        //include the numbe in subset
-        templist.add(nums[i]);
-        //recurssively call other and include them
-        helper(res,templist,nums,i+1);
-        templist.remove(templist.size()-1);
-     }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Set<List<Integer>>res=new HashSet<>();
+        Arrays.sort(nums);
+        helper(0,res,new ArrayList<>(),nums);
+        return new ArrayList<>(res);
     }
 }
