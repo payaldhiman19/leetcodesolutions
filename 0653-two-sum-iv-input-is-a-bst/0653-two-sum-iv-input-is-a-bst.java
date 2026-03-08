1da0521dec5fc7ea3c7dc5cbd1f2a438c90fa310
Payal dhiman <payaldhiman0742@gmail.com>
2026-03-08 21:40:41 +0530

    Time: 3 ms (74.13%), Space: 47.1 MB (91.82%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,20 +14,18 @@
  * }
  */
 class Solution {
-    public boolean twosum(TreeNode root,int k,HashSet<Integer>seen){
-    if(root==null) return false;
-    if(seen.contains(k-root.val)){
-        //if it is present then we have found the pair
-        return true;
-    }
-    //else add in set
-    seen.add(root.val);
-    //if either in left or right we found the pair once we have to return true so we use or operator here
-    return twosum(root.left,k,seen) || twosum(root.right,k,seen);
+    public boolean helper(TreeNode root,int k,HashSet<Integer>set){
+        if(root==null)return false;
+        if(set.contains(k-root.val)){
+            //already present
+            return true;
+        }
+        set.add(root.val);
+        return helper(root.left,k,set)||helper(root.right,k,set);
     }
     public boolean findTarget(TreeNode root, int k) {
-        if(root==null) return false;
+        if(root==null)return false;
         HashSet<Integer>set=new HashSet<>();
-        return twosum(root,k,set);
+        return helper(root,k,set);
     }
 }
