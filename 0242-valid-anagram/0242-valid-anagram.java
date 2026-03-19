ecd5fe23006b4e47d3ea12869a2a8eca692c4929
Payal dhiman <payaldhiman0742@gmail.com>
2026-03-19 10:22:52 +0530

    Time: 6 ms (42.65%), Space: 44.5 MB (72.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    //  HashMap<Character,Integer>map=new HashMap<>();
-    // if(s.length()!=t.length()){
-    //     return false;
-    // }
-    //  for(char ch:s.toCharArray()){
-    //     map.put(ch,map.getOrDefault(ch,0)+1);
-    //  }
-    //  for(char ch:t.toCharArray()){
-    //     if(!map.containsKey(ch)){
-    //         return false; 
-    //     }
-    //     map.put(ch,map.get(ch)-1);   //decrease the character count
-    //     if(map.get(ch)==0){
-    //         map.remove(ch); //if count becomes zero remove it from map;
-    //     }
-    //  }
-    //  return map.isEmpty();
-    
-    char ch1[]=s.toCharArray();
-    char ch2[]=t.toCharArray();
-    if(ch1.length!=ch2.length){
-        return false;
+        if(s.length()!=t.length())return false;
+    int freq[]=new int[26];
+    for(int i=0;i<s.length();i++){
+        freq[s.charAt(i)-'a']++;
+        freq[t.charAt(i)-'a']--;
     }
-    Arrays.sort(ch1);
-    Arrays.sort(ch2);
-    return Arrays.equals(ch1,ch2);
+       for(int cnt:freq){
+        if(cnt!=0)return false;
        }
+       return true;
+     }
 }
