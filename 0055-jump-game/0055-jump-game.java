526741d686f126708da8a5caa748624ef6f1f877
Payal dhiman <payaldhiman0742@gmail.com>
2026-03-20 17:03:39 +0530

    Time: 2 ms (85.54%), Space: 48.1 MB (17.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,11 @@
 class Solution {
-    public boolean helper(int arr[],int idx,Boolean dp[]){
-        if(idx==arr.length-1){
-            return true;
-        }
-        if(arr.length<=idx){
-            return false;
-        }
-        if(dp[idx]!=null){
-            return dp[idx];
-        }
-        int steps=arr[idx];
-        for(int i=1;i<=steps;i++){
-            if(helper(arr,idx+i,dp)) return dp[idx]= true;
-        }
-      return   dp[idx]= false;
-    }
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        Boolean dp[]=new Boolean[n];
-     return helper(nums,0,dp);
+        int maxreach=0;
+        for(int i=0;i<n;i++){
+            if(i>maxreach)return false;
+            maxreach=Math.max(maxreach,i+nums[i]);
+        }
+     return true;
     }
 }
