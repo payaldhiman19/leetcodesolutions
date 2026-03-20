e0c01ec83d7006d4203bcc4e1c41297aca16d9c5
Payal dhiman <payaldhiman0742@gmail.com>
2026-03-20 17:13:19 +0530

    Time: 7 ms (97.88%), Space: 65.2 MB (84.63%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -9,24 +9,23 @@ class Solution {
         }
         return true;
     }
-    public void solve(String s,int index,List<String>path,List<List<String>>ans){
-       if(index==s.length()){
-        ans.add(new ArrayList<>(path));
-        return;
-       }
-       for(int end=index;end<s.length();end++){
-        String sub=s.substring(index,end+1);
-        if(palindrome(sub)){
-            path.add(sub);
-            //recurse backtrack
-            solve(s,end+1,path,ans);
-            path.remove(path.size()-1);
+    public void solve(int index,List<String>path,List<List<String>>ans,String s){
+        if(index==s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
         }
-       }
+        for(int end=index;end<s.length();end++){
+            String sub=s.substring(index,end+1);
+            if(palindrome(sub)){
+                path.add(sub);
+                solve(end+1,path,ans,s);
+                path.remove(path.size()-1);
+            }
+        }
     }
     public List<List<String>> partition(String s) {
-       List<List<String>>ans=new ArrayList<>();
-       solve(s,0,new ArrayList<>(),ans);
-       return ans; 
+        List<List<String>>ans=new ArrayList<>();
+        solve(0,new ArrayList<>(),ans,s);
+        return ans;
     }
 }
