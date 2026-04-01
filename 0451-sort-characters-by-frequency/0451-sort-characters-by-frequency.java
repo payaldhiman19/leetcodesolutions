499bf60f79c66acd5a2b4ba08764e874e29315e2
Payal dhiman <payaldhiman0742@gmail.com>
2026-04-01 10:44:14 +0530

    Time: 13 ms (74.07%), Space: 46.6 MB (59.78%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,30 +1,20 @@
 class Solution {
     public String frequencySort(String s) {
-        // Step 1: Frequency Map
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+       Map<Character,Integer>map=new HashMap<>();
+       for(char ch:s.toCharArray()){
+        map.put(ch,map.getOrDefault(ch,0)+1);
+       } 
+       PriorityQueue<Map.Entry<Character,Integer>>pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+       pq.addAll(map.entrySet());
+       StringBuilder sb=new StringBuilder();
+       while(!pq.isEmpty()){
+        Map.Entry<Character,Integer>mp=pq.poll();
+        char ch=mp.getKey();
+        int freq=mp.getValue();
+        for(int i=0;i<freq;i++){
+            sb.append(ch);
         }
-
-        // Step 2: Max Heap (PriorityQueue) by frequency
-        PriorityQueue<Map.Entry<Character, Integer>> pq =
-            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-
-        pq.addAll(freqMap.entrySet());
-
-        // Step 3: Build the answer string
-        StringBuilder ans = new StringBuilder();
-        while (!pq.isEmpty()) {
-            Map.Entry<Character, Integer> entry = pq.poll();
-            char ch = entry.getKey();
-            int freq = entry.getValue();
-
-            // Append the character 'freq' times
-            for (int i = 0; i < freq; i++) {
-                ans.append(ch);
-            }
+       }
+        return sb.toString();
         }
-
-        return ans.toString();
-    }
 }
