6e263ca625cdc752fc7bb9e0d10f0519e272c33e
Payal dhiman <payaldhiman0742@gmail.com>
2026-04-22 15:03:53 +0530

    Time: 1 ms (63.13%), Space: 43.3 MB (53.1%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,8 +3,8 @@ class Solution {
         if(strs.length==0)return "";
         int i=0;
         Arrays.sort(strs);
-        String first=strs[0]; String last=strs[strs.length-1];
-        while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)){
+        String first=strs[0];String last=strs[strs.length-1];
+        while(i<first.length()&&i<last.length()&&first.charAt(i)==last.charAt(i)){
             i++;
         }
         return first.substring(0,i);
