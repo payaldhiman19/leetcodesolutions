1cbbfa7c11d9f62d00e2fde847fea35ce98f6b83
Payal dhiman <payaldhiman0742@gmail.com>
2026-04-23 10:28:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (81.7%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,27 +10,25 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null ||head.next==null||k==0){
+        if(head==null||head.next==null||k==0){
             return head;
         }
-        //first find the length to find where to cut the list
         int length=1;
         ListNode temp=head;
         while(temp.next!=null){
             temp=temp.next;
             length++;
         }
-         k=k%length;
-    if(k==0)return head;
-        temp.next=head;   //make it circualr
-       
+       k=k%length;
+       if(k==0)return head;
+       temp.next=head;
        int n=length-k;
        ListNode newtail=head;
-       for(int i=1;i<n;i++){
-        newtail=newtail.next;
-       }
-       ListNode newhead=newtail.next;
-       newtail.next=null;
+        for(int i=1;i<n;i++){
+            newtail=newtail.next;
+        }
+        ListNode newhead=newtail.next;
+        newtail.next=null;
         return newhead;
     }
 }
