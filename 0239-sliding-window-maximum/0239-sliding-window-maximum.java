a5c2bc8819639b9441aefa2e6462512959287af0
Payal dhiman <payaldhiman0742@gmail.com>
2026-05-04 12:52:45 +0530

    Time: 32 ms (41.07%), Space: 146.7 MB (63.96%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,33 +1,25 @@
 class Solution {
-    public int[] nge(int nums[]){
-        int n=nums.length;
-        int res[]=new int[n];
-        Stack<Integer>st=new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() &&nums[st.peek()]<=nums[i]){
-                st.pop();
-            }
-            if(st.isEmpty()) res[i]=n;
-            else res[i]=st.peek();
-            st.push(i);
-        }
-        return res;
-    }
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        int ans[]=new int [n-k+1];
-        int nges[]=nge(nums);
-        for(int i=0;i<=n-k;i++){
-            //make sure j is within the window
-            //jump to next greater elemnt and if the index is wothin the window then it is correct
-            int j=i;
-            while(nges[j]<i+k){
-                //to check if it is in range
-                j=nges[j]; //store index of next greater elemnt
-            }
-            //nums of j is maximum in window
-            ans[i]=nums[j];
+        if(n==0)return new int[0];
+        int res[]=new int[n-k+1];
+        Deque<Integer>dq=new ArrayDeque<>();
+        int index=0;
+        for(int i=0;i<n;i++){
+          while(!dq.isEmpty()&&dq.peekFirst()==i-k){
+            //out of window
+            dq.pollFirst();
+          }
+          //if smaller than current remove from last as there is no use then
+          while(!dq.isEmpty()&&nums[i]>nums[dq.peekLast()]){
+            dq.pollLast();
+          }
+          dq.offerLast(i);
+          //if window complete
+          if(i>=k-1){
+            res[index++]=nums[dq.peekFirst()];
+          }
         }
-        return ans;
+        return res;
     }
 }
