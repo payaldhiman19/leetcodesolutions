d28ed744781a79bfe8eea85c56f6a46603abbdf3
Payal dhiman <payaldhiman0742@gmail.com>
2026-05-18 20:02:16 +0530

    Time: 3 ms (28.54%), Space: 119.6 MB (24.68%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -5,18 +5,18 @@ class Solution {
         //greedy problem -- if we need to travel from one staion to another it will be like gas[i]-cost[i]   moving also means we are using cost so that's why differnece
         //if totalcost<totalgas not possible to travel whole 
         //when at any station if it becomes negative, reset to zero coz at current start and next index it will reduce more
-       for(int i=0;i<gas.length;i++){
-        totalgas+=gas[i];
-        totalcost+=cost[i];
-        tank+=gas[i]-cost[i];
-        if(tank<0){
-            start=i+1;
-            tank=0;
-        }
-       }
-        if(totalgas<totalcost){
-            return -1;
-       }
-       return start;
+   for(int i=0;i<gas.length;i++){
+    totalgas+=gas[i];
+    totalcost+=cost[i];
+    tank+=gas[i]-cost[i];
+    if(tank<0){
+        start=i+1;
+        tank=0;
+    }
+   }
+    if(totalgas<totalcost){
+        return -1;
+    }
+    return start;
     }
 }
