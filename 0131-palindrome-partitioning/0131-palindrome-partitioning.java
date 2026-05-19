4bbb04f7d8245a0f053e97f0910d8a5730a37f4e
Payal dhiman <payaldhiman0742@gmail.com>
2026-05-19 19:17:46 +0530

    Time: 8 ms (73.37%), Space: 65.2 MB (55.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -9,7 +9,7 @@ class Solution {
         }
         return true;
     }
-    public void solve(int index,List<String>path,List<List<String>>ans,String s){
+    public void solve(String s,int index,List<String>path,List<List<String>>ans){
         if(index==s.length()){
             ans.add(new ArrayList<>(path));
             return;
@@ -18,14 +18,14 @@ class Solution {
             String sub=s.substring(index,end+1);
             if(palindrome(sub)){
                 path.add(sub);
-                solve(end+1,path,ans,s);
+                solve(s,end+1,path,ans);
                 path.remove(path.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
-        List<List<String>>ans=new ArrayList<>();
-        solve(0,new ArrayList<>(),ans,s);
+        List<List<String>>ans =new ArrayList<>();
+        solve(s,0,new ArrayList<>(),ans);
         return ans;
     }
 }
