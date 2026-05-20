3d5313229ba6f328d5a78557dcc3a2c2dc91c695
Payal dhiman <payaldhiman0742@gmail.com>
2026-05-20 18:07:01 +0530

    Time: 37 ms (71.9%), Space: 46.1 MB (54.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,13 +3,13 @@ class Solution {
         int n=nums.length;
         int dp[]=new int[n];
         for(int i=0;i<n;i++){
-            dp[i]=1;//initailze with 1
+            dp[i]=1;
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
-                dp[i]=Math.max(dp[i],dp[j]+1);
-            }
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
             }
         }
         int ans=0;
