3eb88226d117cb192ae6031960a7a7cd663ea13a
Payal dhiman <payaldhiman0742@gmail.com>
2026-05-21 18:03:46 +0530

    Time: 3 ms (29.05%), Space: 45.3 MB (92.92%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,30 +15,24 @@
  */
 class Solution {
     int preindex=0;
-    public int rootsearch(int []in,int start,int end,int ele){
-        for(int i=start;i<=end;i++){
-            if(in[i]==ele){
+    public int leftright(int []inorder,int val,int st,int end){
+        for(int i=st;i<=end;i++){
+            if(inorder[i]==val){
                 return i;
             }
         }
         return -1;
     }
-    public TreeNode build(int pre[],int in[],int start,int end){
-        if(start>end){
-            return null;
-        }
-        TreeNode root=new TreeNode(pre[preindex]);
-        preindex++;
-        int index=rootsearch(in,start,end,root.val);
-        root.left=build(pre,in,start,index-1);
-        root.right=build(pre,in,index+1,end);
-        
-       
-        return root;
-
+    public TreeNode solve(int []inorder,int []preorder,int st,int end){
+        if(st>end)return null;
+        TreeNode root=new TreeNode(preorder[preindex++]);
+        int index=leftright(inorder,root.val,st,end);
+        root.left=solve(inorder,preorder,st,index-1);
+        root.right=solve(inorder,preorder,index+1,end);
+        return  root;
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         preindex=0;
-        return build(preorder,inorder,0,inorder.length-1);
-            }
+       return solve(inorder,preorder,0,inorder.length-1);
+    }
 }
