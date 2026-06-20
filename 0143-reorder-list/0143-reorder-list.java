a0e3ee185ab7e73fc6a6cc2213150d2a87c1ad5e
Payal dhiman <payaldhiman0742@gmail.com>
2026-06-20 11:03:45 +0530

    Time: 2 ms (88.36%), Space: 48.9 MB (93.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,35 +10,33 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null||head.next==null)return ;
-        //step 1--find middle
+        if(head==null||head.next==null)return;
+        //find middle
         ListNode slow=head;
         ListNode fast=head;
-        while(fast!=null&&fast.next!=null){
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-        }
-        //step 2--reverse second half
-        ListNode second=slow.next;
-        slow.next=null;
-        ListNode prev=null;
-        while(second!=null){
-            ListNode nextt=second.next;
-            second.next=prev;
-            prev=second;
-            second=nextt;
-        }
-        //now prev==new head after reversing
-        ListNode first=head;
-        second=prev;
-        //now merge alternatively
-        while(second!=null){
-            ListNode t1=first.next;
-            ListNode t2=second.next;
-            first.next=second;
-            second.next=t1;
-            first=t1;
-            second=t2;
-        }
+            }
+            //reverse the second half
+            ListNode second=slow.next;
+            slow.next=null;
+            ListNode prev=null;
+            while(second!=null){
+                ListNode nextt=second.next;
+                second.next=prev;
+                prev=second;
+                second=nextt;
+            }
+            ListNode first=head;
+            second=prev;
+            while(second!=null){
+                ListNode t1=first.next;
+                ListNode t2=second.next;
+                first.next=second;
+                second.next=t1;
+                first=t1;
+                second=t2;
+            }
     }
 }
