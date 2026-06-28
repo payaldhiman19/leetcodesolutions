154e9962cd8c9c399254d3cc266f056f29e8c05c
Payal dhiman <payaldhiman0742@gmail.com>
2026-06-28 12:43:54 +0530

    Time: 150 ms (37.99%), Space: 43 MB (59.45%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean solve(char[][]board,String word,int index,int i,int j,int vis[][]){
-        if(index==word.length()){
-            return true;
-        }
-        if(i<0||j<0||i>=board.length||j>=board[0].length||vis[i][j]==1||board[i][j]!=word.charAt(index)){
-            return false;
-        }
-        vis[i][j]=1;
-        boolean found=solve(board,word,index+1,i+1,j,vis)||
-        solve(board,word,index+1,i-1,j,vis)||
-        solve(board,word,index+1,i,j+1,vis)||
-        solve(board,word,index+1,i,j-1,vis);
-        vis[i][j]=0;
-        return found;
+    public boolean issafe(char[][]board,int index,int i,int j,String word,int vis[][]){
+      if(index==word.length()){
+        return true;
+      }
+      if(i<0||j<0||i>=board.length||j>=board[0].length||vis[i][j]==1||board[i][j]!=word.charAt(index)){
+        return false;
+      }
+      vis[i][j]=1;
+      boolean found=issafe(board,index+1,i+1,j,word,vis)||
+      issafe(board,index+1,i-1,j,word,vis)||
+      issafe(board,index+1,i,j+1,word,vis)||
+      issafe(board,index+1,i,j-1,word,vis);
+      vis[i][j]=0;
+      return found;
     }
     public boolean exist(char[][] board, String word) {
         int n=board.length;
@@ -21,7 +21,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(solve(board,word,0,i,j,vis)){
+                    if(issafe(board,0,i,j,word,vis)){
                         return true;
                     }
                 }
