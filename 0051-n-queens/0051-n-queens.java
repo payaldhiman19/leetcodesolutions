3c2477c5d13096ebc0242507a4bc4be823660793
Payal dhiman <payaldhiman0742@gmail.com>
2026-06-29 11:53:22 +0530

    Time: 3 ms (42.19%), Space: 46.7 MB (45.81%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,35 +1,35 @@
 class Solution {
-    public boolean issafe(boolean board[][],int row,int col,int n){
-        for(int i=0;i<row;i++){
-            if(board[i][col])return false;
-        }
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if(board[i][j])return false;
-        }
-        for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
-            if(board[i][j])return false;
-        }
-        return true;
+    public boolean safe(boolean b[][],int n,int row,int col){
+     for(int i=0;i<row;i++){
+        if(b[i][col])return false;
+     }
+     for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+        if(b[i][j])return false;
+     }
+     for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
+        if(b[i][j])return false;
+     }
+     return true;
     }
-    public void solve(boolean board[][],int row,int n,List<List<String>>ans){
+    public void solve(boolean b[][],int row,int n,List<List<String>>ans){
         if(row==n){
-            ans.add(printboard(board));
+            ans.add(printboard(b));
             return;
         }
         for(int col=0;col<n;col++){
-            if(issafe(board,row,col,n)){
-                board[row][col]=true;
-            solve(board,row+1,n,ans);
-                board[row][col]=false;//backtrack
-            }
+            if(safe(b,n,row,col)){
+         b[row][col]=true;
+         solve(b,row+1,n,ans);
+         b[row][col]=false;
+        }
         }
     }
-    public List<String>printboard(boolean board[][]){
+    public List<String>printboard(boolean b[][]){
         List<String>res=new ArrayList<>();
-        for(int i=0;i<board.length;i++){
+        for(int i=0;i<b.length;i++){
             StringBuilder sb=new StringBuilder();
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]){
+            for(int j=0;j<b[0].length;j++){
+                if(b[i][j]){
                     sb.append('Q');
                 }else{
                     sb.append('.');
@@ -41,8 +41,8 @@ class Solution {
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>>ans=new ArrayList<>();
-        boolean board[][]=new boolean[n][n];
-        solve(board,0,n,ans);
+        boolean b[][]=new boolean[n][n];
+        solve(b,0,n,ans);
         return ans;
     }
 }
